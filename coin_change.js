6de7cf5fd1dc429b2a443c09ff8ab2e97ca0b62b
Daniel <daniel.j.fletcher2@gmail.com>
2017-07-28 18:12:19 -0700

    Complete coin change problem

--- coin_change.js
+++ coin_change.js
@@ -1,26 +1,15 @@
-// Source: https://www.hackerrank.com/challenges/ctci-coin-change
-
-function makeChange(target, coins, memo = {}) {
-  if (target in memo) {
-    return memo[target];
-  } else if (target < coins[0]) {
-    memo[target] = 0;
-    return 0;
-  } else if (target === coins[0]) {
-    memo[target] = 1;
-    return 1;
-  } else {
-    memo[target] = 0;
-    coins.forEach(coin => {
-      let combos = makeChange(target - coin, coins, memo);
-      if (combos > 0) {
-        memo[target] += memo[target - coin] + 1;
+// // Source: https://www.hackerrank.com/challenges/ctci-coin-change
+function makeChange(target, coins) {
+  let combos = [1];
+  for (let i = 0; i < target; i++) { combos.push(0); }
+  coins.forEach(coin => {
+    for (let amt = 1; amt <= target; amt++) {
+      if (coin <= amt) {
+        combos[amt] += combos[amt - coin];
       }
-    });
-    console.log(memo);
-    return memo[target];
-  }
+    }
+  });
+  return combos[target];
 }
 
-
-console.log(makeChange(4, [1, 2, 3]));
+console.log(makeChange(10, [6, 2, 3, 5]) === 5);
